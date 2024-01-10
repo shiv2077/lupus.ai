b8781467d938d08a5729ae6dcf729d36a596ed7f
shiv <shivnarayan4077@gmail.com>
2024-01-10 23:51:19 +0530

    main code

---
+++ lupus_app/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:lupus_app/screens/stats.dart';
+import 'package:lupus_app/screens/dashboard2.dart';
+import 'package:lupus_app/screens/login.dart';
+import 'package:lupus_app/screens/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => SplashScreen(),
+          '/login': (context) => Login(),
+          '/dash': (context) => DashboardPage(),
+          '/stats': (context) => StatisticsPage(),
+        });
+  }
+}
